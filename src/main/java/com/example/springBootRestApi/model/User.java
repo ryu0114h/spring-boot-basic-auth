b281527356acd5da2c227d9152fb4aba2adc2986
星野龍 <ryu0114h@gmail.com>
2022-05-04 14:53:27 +0900

    update user model

--- src/main/java/com/example/springBootRestApi/model/User.java
+++ src/main/java/com/example/springBootRestApi/model/User.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity
@@ -15,9 +16,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String name;
 
+    @NotBlank
     private int age;
 
+    @NotBlank
     private String address;
 }
