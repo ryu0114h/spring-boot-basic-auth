7c5adcbf7f98a6fadbfb5df17b41a3f2232b3ccc
星野龍 <ryu0114h@gmail.com>
2022-05-05 14:07:43 +0900

    create employee model

--- src/main/java/com/example/springBootRestApi/model/Employee.java
+++ src/main/java/com/example/springBootRestApi/model/Employee.java
@@ -1,2 +1,23 @@
-package com.example.springBootRestApi.model;public class Employee {
+package com.example.springBootRestApi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import lombok.Data;
+
+@Entity
+@Table(name = "employee")
+@Data
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    private String email;
+
+    private String password;
 }
