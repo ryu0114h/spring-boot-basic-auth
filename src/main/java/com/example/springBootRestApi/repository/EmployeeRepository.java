639135c90bee3a88d3f89e327a5d5acc2127133c
星野龍 <ryu0114h@gmail.com>
2022-05-05 14:07:18 +0900

    create EmployeeRepository

--- src/main/java/com/example/springBootRestApi/repository/EmployeeRepository.java
+++ src/main/java/com/example/springBootRestApi/repository/EmployeeRepository.java
@@ -1,9 +1,13 @@
 package com.example.springBootRestApi.repository;
 
-import com.example.springBootRestApi.model.User;
+import com.example.springBootRestApi.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Employee findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
