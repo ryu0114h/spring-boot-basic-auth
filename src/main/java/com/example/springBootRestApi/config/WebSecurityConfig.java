9fc00a807afcfbcd7891cdd8cb0166f578bddff1
星野龍 <ryu0114h@gmail.com>
2022-05-05 11:48:37 +0900

    add spring-security

---
+++ src/main/java/com/example/springBootRestApi/config/WebSecurityConfig.java
@@ -0,0 +1,2 @@
+package com.example.springBootRestApi.config;public class WebSecurityConfig {
+}
---
+++ src/main/java/com/example/springBootRestApi/security/UserDetailsServiceImpl.java
@@ -0,0 +1,2 @@
+package com.example.springBootRestApi.security;public class UserDetailsServiceImpl {
+}
